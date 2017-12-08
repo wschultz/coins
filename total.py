a847c177b7944fbd69928ce5c13dcaf92a90e04e
wschultz <wschultz@twitter.com>
2017-12-08 06:21:42 -0800

    added in totals.py to easily see all of your holdings

---
+++ total.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+currency = "https://api.coinmarketcap.com/v1/ticker/"
+
+r = requests.get(currency)
+results = r.json()
+
+### START EDIT HERE ###
+omg = 84.4215   # Bittrex
+btc = 2.1435370 # GDAX
+xrp = 6328.8837 # Bitstamp
+
+myholdings = {"omg": omg, "btc": btc, "xrp": xrp}
+#### END EDIT HERE ####
+
+totals = []
+total = 0
+
+for i in results:
+  if str(i['symbol']).lower() in myholdings.keys():
+    for k,v in myholdings.items():
+      if k == str(i['symbol']).lower():
+        totals.append(float(i['price_usd']) * v)
+
+for t in totals:
+  total += t
+
+print(total)
+
